8ca0c1dd7e68fd3d959b59d8955c42237395fee6
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-04-03 16:59:42 +0200

    Random ints + bubbleSort

---
+++ bubbleSort.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var tab [10]int
+
+	for i := 0; i < 10; i++ {
+		tab[i] = random.Intn(10)
+	}
+
+	fmt.Println("Before")
+	fmt.Println(tab)
+
+	for i := 0; i < 10; i++ {
+		for j := 1; j < 10 - i; j ++ {
+			if tab[j - 1] > tab[j] {
+				tmp := tab[j - 1]
+				tab[j - 1] = tab[j]
+				tab[j] = tmp
+			}
+		}
+	}
+
+	fmt.Println("After")
+	fmt.Println(tab)
+
+}
