2eaef811b9201c25a6ae75b543cbc6608b0a5ca9
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-04-03 16:21:16 +0200

    Fibonacci normal + with GO workers!

---
+++ fibonacci.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i := 0
+
+	fmt.Println("Which element of fibonacci sequence?")
+	fmt.Scanf("%d", &i)
+
+	fmt.Println(fibonacci(i))
+
+}
+
+func fibonacci(n int)int {
+	if n < 3 {
+		return 1
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2)
+}
