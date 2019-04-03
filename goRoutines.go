8d6584b355d98aa722e7daaf2192d6a33a55b700
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-04-03 16:45:05 +0200

    Siple go routines

---
+++ goRoutines.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+
+	chan1 := make(chan string)
+	chan2 := make(chan string)
+
+	go func() {
+
+		for {
+			chan1 <- "Every 500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+
+	}()
+
+	go func() {
+
+		for {
+			chan2 <- "Every 2 seconds"
+			time.Sleep(time.Second * 2)
+		}
+
+	}()
+
+	for {
+
+		select {
+			case message1 := <- chan1:
+				fmt.Println(message1)
+			case message2 := <- chan2:
+				fmt.Println(message2)
+		}
+
+	}
+
+}
